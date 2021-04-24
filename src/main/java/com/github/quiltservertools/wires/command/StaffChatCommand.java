f58b40a170f5e2e127c768b1658f50257141ada2
yitzy299 <yitzy299@noreply.github.com>
2021-04-24 21:29:08 +0100

    Feedback for staffchat command

--- src/main/java/com/github/quiltservertools/wires/command/StaffChatCommand.java
+++ src/main/java/com/github/quiltservertools/wires/command/StaffChatCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -20,6 +21,7 @@ public class StaffChatCommand {
                 .then(argument("message", StringArgumentType.greedyString()).executes(
                         ctx -> {
                             Wires.config.getStaffChat().sendMessage(ctx.getSource().getPlayer(), StringArgumentType.getString(ctx, "message"));
+                            ctx.getSource().sendFeedback(new LiteralText(Wires.config.getStaffChat().isInStaffChat(ctx.getSource().getPlayer().getUuid()) ? "Joined" : "Left" + " staffchat"), false);
                             return 1;
                         }
                 )));
